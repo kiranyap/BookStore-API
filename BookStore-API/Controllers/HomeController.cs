6cde11c4dd6db86adfd0eefcd984da8bef6118e4
wei ping <kiranyap25@gmail.com>
2020-09-20 15:39:35 +0800

    created Date classes and DTOs, Also included automapper and change appsettings file.

---
+++ BookStore-API/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore_API.Controllers
+{
+    /// <summary>
+    /// This is a Test API controller
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+    }
+}
