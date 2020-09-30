7cc546c100d5958b67567af8110d1ab70629121b
wei ping <kiranyap25@gmail.com>
2020-09-30 22:51:39 +0800

    Implemented CRUD from Book API Controller and standardized log messages

---
+++ BookStore-API/Controllers/BooksController.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using BookStore_API.Contracts;
+using BookStore_API.Data;
+using BookStore_API.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore_API.Controllers
+{
+    /// <summary>
+    /// Interacts with the Books Table
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookRepository _bookRepository;
+        private readonly ILoggerService _logger;
+        private readonly IMapper _mapper;
+        public BooksController(IBookRepository bookRepository,
+            ILoggerService logger,
+            IMapper mapper)
+        {
+            _bookRepository = bookRepository;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        /// <summary>
+        /// Get All Books
+        /// </summary>
+        /// <returns>Get a list of books</returns>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetBooks()
+        {
+            var location = GetControllerActionNames();
+            try
+            {
+                var books = await _bookRepository.FindAll();
+                var response = _mapper.Map<IList<BookDTO>>(books);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(500);
+            }
+        }
+
+
+        /// <summary>
+        /// Get a Book by Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>book</returns>
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetBook(int id)
+        {
+            var location = GetControllerActionNames();
+            try
+            {
+                var book = await _bookRepository.FindById(id);
+                if (book == null)
+                {
+                    return NotFound();
+                }
+                var response = _mapper.Map<BookDTO>(book);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500);
+            }
+        }
+
+        /// <summary>
+        /// Create a book
+        /// </summary>
+        /// <param name="bookCreateDTO"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Create([FromBody] BookCreateDTO bookCreateDTO)
+        {
+            var location = GetControllerActionNames();
+            try
+            {
+                if (bookCreateDTO == null)
+                {
+                    return BadRequest();
+                }
+
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest();
+                }
+
+                var book = _mapper.Map<Book>(bookCreateDTO);
+                var isSuccess = await _bookRepository.Create(book);
+
+                if (!isSuccess)
+                {
+                    return StatusCode(500);
+                }
+                return Created("Create", new { book });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500);
+            }
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Update(int id, [FromBody] BookUpdateDTO bookDTO)
+        {
+            var location = GetControllerActionNames();
+            try
+            {
+                if(id < 1 || bookDTO == null || id != bookDTO.Id)
+                {
+                    return BadRequest();
+                }
+
+                var isExists = await _bookRepository.isExist(id);
+                if(!isExists)
+                {
+                    return NotFound();
+                }
+
+                if(!ModelState.IsValid)
+                {
+                    return BadRequest();
+                }
+
+                var book = _mapper.Map<Book>(bookDTO);
+                var isSuccess = await _bookRepository.Update(book);
+                if(!isSuccess)
+                {
+                    return StatusCode(500);
+                }
+                return NoContent();
+
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500);
+            }
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var location = GetControllerActionNames();
+            try
+            {
+                if(id < 1)
+                {
+                    return BadRequest();
+                }
+
+                var isExists = await _bookRepository.isExist(id);
+                if(!isExists)
+                {
+                    return NotFound();
+                }
+
+                var book = await _bookRepository.FindById(id);
+                var isSuccess = await _bookRepository.Delete(book);
+
+                if(!isSuccess)
+                {
+                    return StatusCode(500);
+                }
+
+                return NoContent();
+
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(500);
+            }
+        }
+
+
+        private string GetControllerActionNames()
+        {
+            var controller = ControllerContext.ActionDescriptor.ControllerName;
+            var action = ControllerContext.ActionDescriptor.ActionName;
+
+            return $"{controller} - {action}";
+        }
+
+    }
+}
--- BookStore-API/Mappings/Maps.cs
+++ BookStore-API/Mappings/Maps.cs
@@ -11,11 +11,13 @@ namespace BookStore_API.Mappings
     public class Maps: Profile
     {
         public Maps()
-        {
-            CreateMap<Book, BookDTO>().ReverseMap();
+        {            
             CreateMap<Author, AuthorDTO>().ReverseMap();
             CreateMap<Author, AuthorCreateDTO>().ReverseMap();
             CreateMap<Author, AuthorUpdateDTO>().ReverseMap();
+            CreateMap<Book, BookDTO>().ReverseMap();
+            CreateMap<Book, BookCreateDTO>().ReverseMap();
+            CreateMap<Book, BookUpdateDTO>().ReverseMap();
         }
     }
 }
