7b1d590922bc37c1517e6b8d6703768517a1de3a
wei ping <kiranyap25@gmail.com>
2020-10-26 22:49:40 +0800

    Added book CRUD operations, Fixed Book API DTO issues and added Toast Notification

---
+++ BookStore-UI/Contracts/IFileUpload.cs
@@ -0,0 +1,17 @@
+﻿using BlazorInputFile;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore_UI.Contracts
+{
+    public interface IFileUpload
+    {
+        public Task UploadFile(IFileListEntry file, string picName);
+        public void UploadFile(IFileListEntry file, MemoryStream msFile, string picName);
+
+        public void RemoveFile(string picName);
+    }
+}
--- BookStore-UI/Startup.cs
+++ BookStore-UI/Startup.cs
@@ -16,6 +16,7 @@ using Blazored.LocalStorage;
 using System.IdentityModel.Tokens.Jwt;
 using BookStore_UI.Providers;
 using Microsoft.AspNetCore.Components.Authorization;
+using Blazored.Toast;
 
 namespace BookStore_UI
 {
@@ -35,6 +36,7 @@ namespace BookStore_UI
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddBlazoredLocalStorage();
+            services.AddBlazoredToast();
             services.AddHttpClient();
             services.AddScoped<ApiAuthenticationStateProvider>();
             services.AddScoped<AuthenticationStateProvider>(p =>
@@ -42,6 +44,8 @@ namespace BookStore_UI
             services.AddScoped<JwtSecurityTokenHandler>();
             services.AddTransient<IAuthenticationRepository, AuthenticationRepository>();
             services.AddTransient<IAuthorRepository, AuthorRepository>();
+            services.AddTransient<IBookRepository, BookRepository> ();
+            services.AddTransient<IFileUpload, FileUpload>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
